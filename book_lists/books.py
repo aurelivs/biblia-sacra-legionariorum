5ed1f49aa418542424f594a03ac793ee01e3fe7e
Aurelius <143103924+aurelivs@users.noreply.github.com>
2023-08-24 19:23:59 -0300

    fix: modules fix

--- book_lists/books.py
+++ book_lists/books.py
@@ -1,6 +1,10 @@
-import categories
+from book_lists.categories.at_1 import antigo_testamento1
+from book_lists.categories.at_2 import antigo_testamento2
+from book_lists.categories.nt_1 import novo_testamento1
+from book_lists.categories.nt_2 import novo_testamento2
 
-at_1 = categories.antigo_testamento1
-at_2 = categories.antigo_testamento2
-nt_1 = categories.novo_testamento1
-nt_2 = categories.novo_testamento2
+
+at_1 = antigo_testamento1
+at_2 = antigo_testamento2
+nt_1 = novo_testamento1
+nt_2 = novo_testamento2
